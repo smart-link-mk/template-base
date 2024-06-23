70a4d1fe6e721ea1ea48f747c05566747eeb2ae7
Muamet Islami <grooby.muamet@gmail.com>
2024-06-23 12:48:55 +0200

    fix zip toBufferPromise getting called for each user

--- publish.js
+++ publish.js
@@ -9,48 +9,50 @@ zip.addLocalFolder("template");
 const [, , ...owners] = process.argv;
 const { name, version } = package;
 
-Promise.all(
-  owners.map(async (apiKey) => {
-    const { jwt } = await fetch(
-      "https://api.smartlink.mk/auth/api/authorize?apiKey=" + apiKey,
-    ).then((r) => r.json());
+zip.toBufferPromise().then((zipBuffer) =>
+  Promise.all(
+    owners.map(async (apiKey) => {
+      const { jwt } = await fetch(
+        "https://api.smartlink.mk/auth/api/authorize?apiKey=" + apiKey,
+      ).then((r) => r.json());
 
-    const { ok: templateCreated } = await fetch(
-      "https://api.smartlink.mk/templates",
-      {
-        method: "post",
+      const { ok: templateCreated } = await fetch(
+        "https://api.smartlink.mk/templates",
+        {
+          method: "post",
 
-        headers: {
-          Authorization: "Bearer " + jwt,
-          "Content-Type": "application/json",
+          headers: {
+            Authorization: "Bearer " + jwt,
+            "Content-Type": "application/json",
+          },
+
+          body: JSON.stringify({ name }),
         },
+      );
 
-        body: JSON.stringify({ name }),
-      },
-    );
+      if (templateCreated) {
+        console.info("Created template: " + name);
+      }
 
-    if (templateCreated) {
-      console.info("Created template: " + name);
-    }
+      const formData = new FormData();
+      const headers = formData.getHeaders();
 
-    const formData = new FormData();
-    const headers = formData.getHeaders();
+      headers["Authorization"] = "Bearer " + jwt;
+      formData.append("jsonSchema", JSON.stringify(schema));
+      formData.append("version", version);
+      formData.append("files[]", zipBuffer, "archive.zip");
 
-    headers["Authorization"] = "Bearer " + jwt;
-    formData.append("jsonSchema", JSON.stringify(schema));
-    formData.append("version", version);
-    formData.append("files[]", await zip.toBufferPromise(), "archive.zip");
+      const res = await fetch(
+        `https://api.smartlink.mk/templates/${name}/versions`,
+        {
+          method: "POST",
+          headers: headers,
+          body: formData,
+        },
+      );
 
-    const res = await fetch(
-      `https://api.smartlink.mk/templates/${name}/versions`,
-      {
-        method: "POST",
-        headers: headers,
-        body: formData,
-      },
-    );
-
-    console.info(res.status, res.statusText);
-  }),
+      console.info(res.status, res.statusText);
+    }),
+  ),
 );
 console.log(name, version, owners);
